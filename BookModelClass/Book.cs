6a530dbe8730264c00a6ca53cea1fe1cd35ea2a9
Ganga6 <44807131+Ganga6@users.noreply.github.com>
2019-10-17 15:12:59 +0200

    Add files

---
+++ BookModelClass/Book.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookModelClass
+{
+   public class Book
+    {  //Instance Field
+       
+        private String _title;
+        private int _pageNumber;
+        private String _isbn13;
+        //Properties
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Characters should be more than two !! ");
+                }
+
+                _title = value;
+            }
+
+
+        }
+        public String Author { get; set; }
+
+        public int PageNumber
+        {
+            get
+            {
+                return _pageNumber;
+            }
+            set
+            {
+                if (value < 10 && value >= 1000)
+                {
+                    throw new ArgumentException("Page number should be greater than 10 and smaller or euals to 1000 ");
+                }
+
+                _pageNumber = value;
+            }
+        }
+
+        public string Isbn13
+        {
+            get
+            {
+                return _isbn13;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    _isbn13 = value;
+                }
+
+                else throw new Exception("it should contain 13 characters");
+            }
+
+        }
+
+        public Book(string title, int pageNumber, string isbn13, string author)
+        {
+            _title = title;
+            _pageNumber = pageNumber;
+            _isbn13 = isbn13;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"title:{Title},author:{Author}, pagenumber:{PageNumber} ,Isnm13:{Isbn13} ";
+        }
+
+    }
+
+}
+
+
---
+++ SimpleRestServiceAssignment/Controllers/BooksController.cs
@@ -0,0 +1,70 @@
+﻿using BookModelClass;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SimpleRestServiceAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public static readonly List<Book> bookList = new List<Book>()
+        {
+            new Book("The girl",300,"nkskskhdb23jw","Ganga"),
+            new Book("the colour ourple",800,"J:K Rolling","162gSuw72gwV3"),
+            new Book("The Alcemist",300,"Shiva Khera","7493GS832YHc2"),
+            new Book("Capital",250 ,"Karl Marx","2648gd837GBS4"),
+            new Book("aama",900,"Laxmi Prashad Devkota","LA12xm19I45po")
+
+        };
+       
+
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return bookList;
+        }
+        // GET: api/Books/5
+        [HttpGet]
+        [Route("{isbn}")]
+        public Book Get(string isbn)
+        {
+            return bookList.Find(i => i.Isbn13== isbn);
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public void Post([FromBody] Book value)
+        {
+            bookList.Add(value);
+        }
+
+        // PUT: api/Books/523hjsdjj232
+        [HttpPut("{isbn}")]
+        public void Put(string isbn, [FromBody] Book value)
+        {
+            Book bok = Get(isbn);
+            if (bok != null)
+            {
+                bok.Title = value.Title;
+                bok.Author = value.Author;
+                bok.PageNumber = value.PageNumber;
+                bok.Isbn13=value.Isbn13;
+            }
+
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{isbn}")]
+        public void Delete(string isbn)
+        {
+            Book bok = Get(isbn);
+            bookList.Remove(bok);
+        }
+
+
+
+    }
+}
